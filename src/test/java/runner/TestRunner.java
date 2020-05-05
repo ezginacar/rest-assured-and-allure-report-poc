803c6548312300025ab4296c23370ad3275bfd47
ezginacar <ncrezgi@gmail.com>
2020-05-05 14:35:13 +0300

    new test class added to the suite

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,22 @@
 package runner;
 
+import newsTest.FailedInvalidSearch;
 import newsTest.FailedViewNewsAfter1MounthFromCurrentDate;
 import newsTest.SuccessfulGettingNews;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+
 
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-        FailedViewNewsAfter1MounthFromCurrentDate.class , SuccessfulGettingNews.class
+        FailedViewNewsAfter1MounthFromCurrentDate.class , SuccessfulGettingNews.class, FailedInvalidSearch.class
 })
 
 public class TestRunner {
+
+
+
 }
